efd5e20e4c8e55234a22e9cff7faede6af115d39
ManituPC <manitupc@yandex.ru>
2019-07-02 22:59:49 +0300

    add general files

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by denis.chicherin on 7/2/19.
+//  Copyright © 2019 LearnAppMaking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
