ef3c7c9951420b1585d5c923496596efc731b0da
ManituPC <manitupc@yandex.ru>
2019-08-14 23:24:40 +0300

    add some func

--- test1122/Models/BaseModels/RootViewController.swift
+++ test1122/Models/BaseModels/RootViewController.swift
@@ -16,5 +16,18 @@ class RootViewController: UIViewController {
       current.view.frame = view.bounds              // 2 size settings            
       view.addSubview(current.view)                 // 3 
       current.didMove(toParentViewController: self) // 4
-   }
+   	}
+
+
+   	func showCitysListScreen() {
+   		let new = UINavigationController(rootViewController: CitysListViewController())
+   		addChildViewController(new)                    // 2
+   		new.view.frame = view.bounds                   // 3
+      	view.addSubview(new.view)                      // 4
+      	new.didMove(toParentViewController: self)      // 5 
+      	current.willMove(toParentViewController: nil)  // 6 подготовка к удалению контроллера
+      	current.view.removeFromSuperview()             // 7 удаление контроллера из родительского
+      	current.removeFromParentViewController()       // 8 удаление из rootViewController
+      	current = new								   // 9 refresh current viewController
+   	}
 }
