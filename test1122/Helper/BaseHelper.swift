c73b7082b92277ec838f4631ed7fc5be38d90ecd
ManituPC <chicherin.dn@gmail.com>
2019-09-17 09:38:41 +0300

    add localizable files

---
+++ test1122/Helper/BaseHelper.swift
@@ -0,0 +1,14 @@
+//
+//  BaseHelper.swift
+//  test1122
+//
+//  Created by denis.chicherin on 9/16/19.
+//  Copyright © 2019 LearnAppMaking. All rights reserved.
+//
+
+import Foundation
+
+
+class BaseHelper {
+    
+}
